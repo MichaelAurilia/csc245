8d1d524247205c1c0679b581ef14df071767a361
MichaelAurilia <Michael.Aurilia@cix.csi.cuny.edu>
2023-02-16 19:24:05 -0500

    This is a commit test

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,13 +3,30 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
+import numpy as np
 
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
     #print('Hello there')
     #print(repeat('Yay', True))
-    stringTester()
+    #stringTester()
+    print(np.array([1, 4, 2, 5, 3]))
+
+    colors = ['red', 'blue', 'green']
+    print(colors)
+    print(len(colors))
+
+    squares = [1, 4, 9, 16]
+    sum = 0
+    for num in squares:
+        sum += num
+        x = lambda a: a + 19
+        x = lambda a, b: a * b
+        print(x(5, 6))
+        i = 0
+        #for i in range(100):
+            #print(i)
 
 
 def repeat(s, exclaim):
@@ -24,6 +41,8 @@ def stringTester():
     print(s[0])
     print(len(s))
     print(s + "there")
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
